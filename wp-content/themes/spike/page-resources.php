fa2e1220f72cfcd7d10ba97e47df2afa0c120edb
Mike <mike@sachsmedia.com>
2016-08-16 17:04:06 -0400

    new template page

---
+++ wp-content/themes/spike/page-resources.php
@@ -0,0 +1,49 @@
+<?php
+	/*
+	 * @package Spike
+	 * Template Name: Resources
+  */
+	get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>				
+					<div class="entry-content contain">
+						<header class="entry-header">
+							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						</header><!-- .entry-header -->
+						<?php the_content(); ?>
+
+						<?php $fields = get_fields();?>
+						
+						<?php foreach ($fields['resources'] as $resource): ?>
+						<div style="text-align:center">
+							<div style="display:inline-block">
+								<a href="<?= $resource['link'] ?>"rel="">
+									<img src="<?= $resource['image'] ?>"/>
+								</a>
+								<h4><a href="<?= $resource['link'] ?>"rel=""><?= $resource['title'] ?></a></h4>
+							</div>
+						</div>
+						<?php endforeach; ?>
+						<?php
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . __( 'Pages:', 'spike' ),
+								'after'  => '</div>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+					<footer class="entry-footer">
+						<?php edit_post_link( __( 'Edit', 'spike' ), '<span class="edit-link">', '</span>' ); ?>
+					</footer><!-- .entry-footer -->
+				</article><!-- #post-## -->
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
